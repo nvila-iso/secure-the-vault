1ecffa88e732c07ca7d07ac6c5e0d145dd394a24
nvila-iso <nvila88@proton.me>
2025-07-23 12:50:43 -0400

    updated js

---
+++ index.js
@@ -0,0 +1,14 @@
+let code1;
+code1 = 7 + 3;
+
+const code2 = 70 / 2 + 5;
+
+const code3 = (80 % 3) + 37;
+
+const message = "The vault has been secured. The combination is:";
+
+const codeA = code1 + "-" + code2 + "-" + code3;
+
+const codeB = `${code1}-${code2}-${code3}`;
+
+console.log(`${message} ${codeA} ${codeB}`);
